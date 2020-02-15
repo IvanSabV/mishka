274e665ef17bc659aed9c3356fda91899fa92b4d
IvanSabV <60038980+IvanSabV@users.noreply.github.com>
2020-02-15 18:39:27 +0400

    mobile header

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,89 @@
-﻿var gulp = require('gulp'),
+﻿"use strict";
+
+var gulp = require('gulp'),
     less = require('gulp-less'),
+    sourcemap = require("gulp-sourcemaps"),
+    rename =  require("gulp-rename"),
     browserSync = require('browser-sync'),
     del = require('del'),
     imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
     cache = require('gulp-cache'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require("gulp-plumber"),
+    webp = require("gulp-webp"),
+    svgstore = require("gulp-svgstore");
 
 gulp.task('less', function () {
-  return gulp.src('source/less/**/*.less')
+  return gulp.src('source/less/main.less')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(less())
     .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('source/css'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(rename("main.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
-
+    notify: false,
   });
+
+  gulp.watch('source/less/**/*.less', gulp.series('less')); // Наблюдение за sass файлами
+  gulp.watch("source/img/icon-*.svg", gulp.series('sprite', 'code'));
+  gulp.watch('source/*.html', gulp.series('code')); // Наблюдение за HTML файлами в корне проекта
+
 });
 
 gulp.task('code', function() {
   return gulp.src('source/*.html')
-  .pipe(browserSync.reload({ stream: true }))
+  .pipe(gulp.dest("build"))
+  .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('clean', async function() {
-  return del.sync('dist'); // Удаляем папку dist перед сборкой
+  return del.sync('build'); // Удаляем папку build перед сборкой
 });
 
 gulp.task('img', function() {
   return gulp.src('source/img/**/*') // Берем все изображения из app
-      .pipe(cache(imagemin({ // С кешированием
-      // .pipe(imagemin({ // Сжимаем изображения без кеширования
-          interlaced: true,
-          progressive: true,
-          svgoPlugins: [{removeViewBox: false}],
-          use: [pngquant()]
-      }))/**/)
-      .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+      .pipe(imagemin([
+        imagemin.optipng({optimizationlevel: 3}),
+        imagemin.mozjpeg({progressive: true}),
+        imagemin.svgo()
+        ]))
+      .pipe(gulp.dest('build/img')); // Выгружаем на продакшен
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore ({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task('prebuild', async function() {
 
-    var buildFonts = gulp.src('source/fonts/**/*') // Переносим шрифты в продакшен
-    .pipe(gulp.dest('source/fonts'))
+    var buildFonts = gulp.src('source/fonts/**/*.{woff,woff2}') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('build/fonts'))
 
     var buildJs = gulp.src('source/js/**/*') // Переносим скрипты в продакшен
-    .pipe(gulp.dest('source/js'))
+    .pipe(gulp.dest('build/js'))
 
     var buildHtml = gulp.src('source/*.html') // Переносим HTML в продакшен
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('build'));
 
 });
 
@@ -62,10 +91,7 @@ gulp.task('clear', function (callback) {
   return cache.clearAll();
 })
 
-gulp.task('watch', function() {
-  gulp.watch('source/less/**/*.less', gulp.parallel('less')); // Наблюдение за sass файлами
-  gulp.watch('source/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
-});
-gulp.task('default', gulp.parallel('less', 'browser-sync', 'watch'));
-gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'less'));
 
+gulp.task('build', gulp.series('prebuild', 'clean', 'img', 'webp', 'less', 'sprite'));
+
+gulp.task('start', gulp.series('build', 'browser-sync'));
